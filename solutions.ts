b18040f5d2b2af7f4fd40d587a7105bd6bc0b928
TanjimulSabbir <tanjimulsabbir.dev@gmail.com>
2026-05-08 22:45:39 +0600

    refactor: update package configuration and implement new features in solutions

--- solutions.ts
+++ solutions.ts
@@ -3,13 +3,28 @@
 // =======================
 function filterEvenNumbers(arr: number[]): number[] {
     const result: number[] = [];
- 
+
     for (let i = 0; i < arr.length; i++) {
-        
-        if (arr[i] % 2 === 0) {
-            result[result.length] = arr[i];
+
+        if (arr[i]! % 2 === 0) {
+            result[result.length] = arr[i]!;
         }
     }
 
     return result;
 }
+
+// =======================
+// Problem 2
+// =======================
+function reverseString(str: string): string {
+    let reversed: string = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed = reversed + str[i]!;
+    }
+
+    return reversed;
+}
+
+console.log(reverseString("type"));
